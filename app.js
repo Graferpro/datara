8d979137aa0aa7293b8339e6b3ab67505d671df5
Graferpro <graferpol@gmail.com>
2026-02-02 08:31:46 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function convert() {
-  const rate = 0.76; // 1 PLN = 0.76 GEL
-  const amount = document.getElementById("amount").value;
-  document.getElementById("result").innerText =
-    (amount * rate).toFixed(2) + " GEL";
-}
